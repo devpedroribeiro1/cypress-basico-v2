7c6c77a489578e468a3291190c0caf215e37a3ac
Pedro Ramos Santos Ribeiro <pedro.ribeiro@narwalsistemas.com.br>
2024-11-28 09:45:22 -0300

    realiza 02.md exercicio

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -8,8 +8,41 @@
 /// <reference types="cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', () => {
+    beforeEach(() => {
+        cy.visit('../../src/index.html');
+    })
+
     it('verifica o título da aplicação', () => {
-        cy.visit('./src/index.html')
-        cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
+        cy.title().should('eq', 'Central de Atendimento ao Cliente TAT');
+    })
+
+    it('preenche os campos obrigatórios e envia o formulário', async () => {
+        const campos = {
+            nome: '#firstName',
+            sobrenome: '#lastName',
+            email: '#email',
+            desc: '#open-text-area'
+        }
+        const preenchimentos = {
+            nome: 'Pedro',
+            sobrenome: 'Ribeiro',
+            email: 'pedro.ribeiro@narwalsistemas.com.br',
+            desc: 'Meu computador está lento, preciso de uma nova placa de vídeo com pelo menos 8gb VRAM GDDR6.'
+        }
+
+        cy.get(campos.nome)
+            .type(preenchimentos.nome);
+        cy.get(campos.sobrenome)
+            .type(preenchimentos.sobrenome);
+        cy.get(campos.email)
+            .type(preenchimentos.email);
+        cy.get(campos.desc)
+            .type(preenchimentos.desc);
+
+        cy.get('button[type="submit"]').click();
+
+        cy.get('span[class="success"]').should('be.visible')
+
+
     })
 })
